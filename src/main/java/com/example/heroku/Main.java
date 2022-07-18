2a1e623a146285f47a3dbb1169e0491ec56a0708
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-07-18 18:39:24 +0200

    Prueba de nuevo con heroku.

--- src/main/java/com/example/heroku/Main.java
+++ src/main/java/com/example/heroku/Main.java
@@ -1,23 +1,15 @@
-package com.example;
+package com.example.heroku;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
- *
- * This class launches the web application in an embedded Jetty container.
- * This is the entry point to your application. The Java command that is used for
- * launching should fire this main method.
- *
+ * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
+ * command that is used for launching should fire this main method.
  */
 public class Main {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) throws Exception{
-        String webappDirLocation = "src/main/webapp/";
-
         // The port that we should run on can be set into an environment variable
         // Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
@@ -25,13 +17,10 @@ public class Main {
             webPort = "8080";
         }
 
-        Server server = new Server(Integer.valueOf(webPort));
-        WebAppContext root = new WebAppContext();
+        final Server server = new Server(Integer.valueOf(webPort));
+        final WebAppContext root = new WebAppContext();
 
         root.setContextPath("/");
-        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
-
         // Parent loader priority is a class loader setting that Jetty accepts.
         // By default Jetty will behave like most web containers in that it will
         // allow your application to replace non-server libraries that are part of the
@@ -39,10 +28,13 @@ public class Main {
         // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
         root.setParentLoaderPriority(true);
 
+        final String webappDirLocation = "src/main/webapp/";
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
         server.setHandler(root);
 
         server.start();
         server.join();
     }
-
 }
