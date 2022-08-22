cdea289f36510985143103a90a98346791c83e89
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-08-22 22:57:53 +0200

    Pequeños cambios.

--- src/main/webapp/docs/swagger-initializer.js
+++ src/main/webapp/docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        url: "http://localhost:8080/docs/swagger.yaml",
+        url: "https://todolist-api1.herokuapp.com/docs/swagger.yaml",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
